98644ee18812a8ca6ff7620ddd0bcbbd1aa0ef52
deturk1 <deg17003@byui.edu>
2019-04-12 09:19:23 -0600

    deturkfoundation

---
+++ charity-help-lite/footer.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * The template for displaying the footer.
+ *
+ * Contains the closing of the #content div and all content after
+ *
+ * @package Charity Help Lite
+ */
+?>
+
+<div class="footer-copyright">
+        	<div class="container">
+            	<div class="copyright-txt">
+					<?php bloginfo('name'); ?>. <?php esc_html_e('All Rights Reserved', 'charity-help-lite');?>           
+                </div>
+                <div class="design-by">
+				  <a href="<?php echo esc_url( __( 'https://gracethemes.com/themes/charity-help-lite/', 'charity-help-lite' ) ); ?>"><?php printf( __( 'Theme by %s', 'charity-help-lite' ), 'Grace Themes' ); ?></a>
+                </div>
+                 <div class="clear"></div>
+            </div>           
+        </div>        
+</div><!--#end site-holder-->
+
+<?php wp_footer(); ?>
+</body>
+</html>
